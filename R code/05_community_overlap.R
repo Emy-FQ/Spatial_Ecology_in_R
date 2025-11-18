2c7d7214824cb87c74398025874cf9d9e3df83b0
Emy-FQ <emy.filippiquintussi@studio.unibo.it>
2025-11-18 16:54:09 +0100

    Update 05_community_overlap.R

--- R code/05_community_overlap.R
+++ R code/05_community_overlap.R
@@ -13,10 +13,20 @@ kerinci$circ<-kerinci$Time*2*pi #the $ links the variable to the data
 head(kerinci)
 
 #subsetting a single species, in this case the tiger
-tiger<-kerinci[kerinci$Sps=="tiger",]#the comma is important to define the colums
+tiger<-kerinci[kerinci$Sps=="tiger",] #== means equal while != means not equal in the sql language #the comma is important to define the colums
 #now we can create an object in which we can put the circualr time for the tiger
 timetiger<-tiger$circ
 
 densityPlot(timetiger)
 #we can see that the times at which the tiger moves more are 6AM and around 6PM
+
+#18/11
+#exercise: repeat the graph for the macaque
+macaque<- kerinci[kerinci$Sps=="macaque",]
+timemacaque<- macaque$circ
+densityPlot(timemacaque)
+#to consider two different times in the data set and compare them, we can use the function "overlapPlot":
+dev.off() #to close the window since we used par before
+overlapPlot(timetiger,timemacaque)
+
 
