2c906ebaa54dfe4cb7e6bee414618c024f5e90f7
Emy-FQ <emy.filippiquintussi@studio.unibo.it>
2025-11-21 10:05:21 +0100

    Create 10_remote_sensing_visualization.R

---
+++ R code/10_remote_sensing_visualization.R
@@ -0,0 +1,8 @@
+#21/11
+#code to visualize remote sensing data
+install.packages("imageRy")
+library(imageRy)
+#all the functions of imageRy start with "im."
+im.list() #this allows us to see al the functions
+#zenodo is a site where we can find opensource data (?)
+#zenodo set: https://zenodo.org/records/15645465
