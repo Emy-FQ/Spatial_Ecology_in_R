b06cc67ed39a5bde1a1cd830d08817dba979c343
Emy-FQ <emy.filippiquintussi@studio.unibo.it>
2025-11-21 09:35:26 +0100

    Update 08_community_graph_theory.R

--- R code/08_community_graph_theory.R
+++ R code/08_community_graph_theory.R
@@ -8,3 +8,14 @@ prey<- c("Algae","Zooplankton","Small Fish","Small Fish","Large Fish")
 #in the prey and predator groups the order is not causual, they are paired, so each prey predator correspond to a prey (e.g. the xooplankton is the predator of the algae etc)
 interactions <- data.frame(predator,prey) #this build a table, a dataset with the predator-prey interactions
 interactions
+
+#21/11
+#we are going to use a function to produce an igraph from a table
+g<- graph_from_data_frame(interactions, vertices=species,directed=TRUE) 
+#the vertices arguments specifies which are the points that are going to appear in the graph (in our case they will be the predators and the preys)
+#directed means that there is a direction from the predators to the preys
+plot(g)
+g<- graph_from_data_frame(interactions, vertices=species,directed=F) #without directions
+plot(g) #there are many potential graphs that we can obtain
+# the set.seed function selects one of the possible graph that we can obtain
+set.seed(42) #the number here is chosen at random
