f94e117f8852262c41fd53634668e5f04ac102f5
Emy-FQ <emy.filippiquintussi@studio.unibo.it>
2026-09-05 12:36:24 +0200

    Update Autumn.js

--- EXAM /JavaScript/Autumn.js
+++ EXAM /JavaScript/Autumn.js
@@ -1,1 +1,13 @@
-
+// select the area of interest on the map, the code of the selection is:
+var AOI = 
+    /* color: #bbffef */
+    /* displayProperties: [
+      {
+        "type": "rectangle"
+      }
+    ] */
+    ee.Geometry.Polygon(
+        [[[-6.620941182520183, 54.76089192999706],
+          [-6.620941182520183, 54.476226465328416],
+          [-6.217193623926433, 54.476226465328416],
+          [-6.217193623926433, 54.76089192999706]]], null, false);
