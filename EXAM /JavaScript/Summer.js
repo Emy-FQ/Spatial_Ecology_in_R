cecd8f35e88e2a12e4dba2b9ec4b4153503bad91
Emy-FQ <emy.filippiquintussi@studio.unibo.it>
2026-09-05 12:35:41 +0200

    Update Summer.js

--- EXAM /JavaScript/Summer.js
+++ EXAM /JavaScript/Summer.js
@@ -1,1 +1,56 @@
+// select the area of interest on the map, the code of the selection is:
+var AOI = 
+    /* color: #bbffef */
+    /* displayProperties: [
+      {
+        "type": "rectangle"
+      }
+    ] */
+    ee.Geometry.Polygon(
+        [[[-6.620941182520183, 54.76089192999706],
+          [-6.620941182520183, 54.476226465328416],
+          [-6.217193623926433, 54.476226465328416],
+          [-6.217193623926433, 54.76089192999706]]], null, false);
 
+// define a function to mask the clouds
+function maskS2cloudsSCL(image) {
+  var scl = image.select('SCL');
+  // select the pixels classes to: keep 4 = Vegetation, 5 = Bare Soils, 6 = Water, 11 = Snow/Ice
+  var keepPixels = scl.eq(4)
+  .or(scl.eq(5))
+  .or(scl.eq(6))
+  .or(scl.eq(11));
+  return image.updateMask(keepPixels);
+}
+
+// Load Sentinel-2 Surface Reflectance data
+var summer_collection = ee.ImageCollection("COPERNICUS/S2_SR_HARMONIZED")
+ .filterBounds(AOI)
+.filterDate('2023-06-01', '2023-08-31') //meterological summer
+.filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE', 3))
+.map(maskS2cloudsSCL);
+
+
+// Check the number of available images 
+print(summer_collection.size()); //there is only one image
+
+// Get the first (and only) image from the filtered collection
+var summer_image = summer_collection.first();
+
+// Select the bands, scale by 10,000, and clip to the AOI
+var processed_image = summer_image.select(['B4', 'B3', 'B2', 'B5', 'B8'])
+  .divide(10000)
+  .clip(AOI);
+
+// Display natural RGB colors
+Map.centerObject(AOI, 10);
+Map.addLayer(processed_image, {bands: ['B4', 'B3', 'B2'], min: 0, max: 0.3}, 'Lough Neagh Summer RGB');
+
+// Export to Google Drive
+Export.image.toDrive({
+  image: processed_image,
+  description: 'Sentinel2_Export',
+  scale: 10,
+  region: AOI,
+  fileFormat: 'GeoTIFF'
+});
